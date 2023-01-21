0fa5c9bef99c761b4bea33df8559bb13006a02f3
Roza Ogannesyan <rozaogannesyan109@gmail.com>
2023-01-22 01:22:53 +0300

    main-375

--- script.js
+++ script.js
@@ -27,6 +27,42 @@ const swiperServices = new Swiper(".swiper-services", {
   scroll: true,
   scrollbar: {
     el: ".swiper-scrollbar__services",
+    //draggable: true,
+    //hide: true,
+  },
+});
+
+const swiperButtons = new Swiper(".products__buttons", {
+  scroll: true,
+});
+const swiperProducts = new Swiper(".swiper-products", {
+  navigation: {
+    nextEl: ".swiper-button-next__products",
+    prevEl: ".swiper-button-prev__products",
+  },
+  //slidesPerView: 1,
+  //slidesPerGroup: 1,
+  spaceBetween: 10,
+  //loop: true,
+  scroll: true,
+  scrollbar: {
+    el: ".swiper-scrollbar__products",
+    draggable: true,
+    //hide: true,
+  },
+});
+const swiperBlog = new Swiper(".swiper-blog", {
+  navigation: {
+    nextEl: ".swiper-button-next__blog",
+    prevEl: ".swiper-button-prev__blog",
+  },
+  //slidesPerView: 1,
+  //slidesPerGroup: 1,
+  spaceBetween: 10,
+  //loop: true,
+  scroll: true,
+  scrollbar: {
+    el: ".swiper-scrollbar__blog",
     draggable: true,
     //hide: true,
   },
